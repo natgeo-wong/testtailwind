18e0ad02f7769a651bbd013faee3cf797576d179
Nathanael Wong <natgeo.wong@outlook.com>
2024-02-29 22:24:52 -0500

    And again

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/tBLOG${src}`} {...rest} />
+const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/BLOG${src}`} {...rest} />
 
 export default Image
